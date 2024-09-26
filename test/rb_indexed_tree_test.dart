cc320057bff72fe70dba609e762d6db8a67103de
lch361 <author@lch361.net>
2024-09-26 23:21:01 +0300

    Улучшил тесты с помощью setUp и tearDown

--- test/rb_indexed_tree_test.dart
+++ test/rb_indexed_tree_test.dart
@@ -17,40 +17,46 @@ void main() {
       expect(rb.insert(7), 7);
     });
 
+    var rb = RbIndexedTree<num>();
+
+    setUp(() {
+      rb.insert(1);
+      rb.insert(5);
+      rb.insert(4);
+      rb.insert(6);
+      rb.insert(2);
+      rb.insert(3);
+      rb.insert(0);
+      rb.insert(9);
+      rb.insert(8);
+      rb.insert(7);
+    });
+
+    tearDown(() {
+      rb = RbIndexedTree();
+    });
+
     test('Length', () {
-      var rb = RbIndexedTree<num>();
-      expect(rb.length, 0);
+      expect(rb.length, 10);
 
       rb.insert(1);
       rb.insert(5);
       rb.insert(4);
-      expect(rb.length, 3);
+      expect(rb.length, 13);
 
       rb.insert(6);
       rb.insert(2);
       rb.insert(3);
       rb.insert(0);
-      expect(rb.length, 7);
+      expect(rb.length, 17);
 
       rb.insert(9);
       rb.insert(8);
       rb.insert(7);
-      expect(rb.length, 10);
+      expect(rb.length, 20);
     });
 
     test('Indices', () {
-      var rb = RbIndexedTree<num>();
-      rb.insert(1);
-      rb.insert(5);
-      rb.insert(4);
-      rb.insert(6);
-      rb.insert(2);
-      rb.insert(3);
-      rb.insert(0);
-      rb.insert(9);
-      rb.insert(8);
-      rb.insert(7);
-
       for (int i = 0; i < 10; ++i) {
         expect(rb[i], i);
       }

5f0e53ae9f235ed759ce0e36428ced4677bc3367
lch361 <author@lch361.net>
2024-10-28 03:14:59 +0300

    Добавил экспортер формата SRT

---
+++ lib/editor/export/srt.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:subtitle_editor/editor/subtitles.dart';
+import 'package:subtitle_editor/editor/time.dart';
+
+void _writeDoubleDigitInt(RandomAccessFile file, int value) {
+  const zero = 0x30; // 0
+  final result = value.toString();
+  if (result.length == 1) {
+    file.writeByteSync(zero);
+  }
+  file.writeStringSync(result);
+}
+
+void _writeTime(RandomAccessFile file, MillisFormatted value) {
+  const colon = 0x3a /* : */, comma = 0x2c /* , */;
+  _writeDoubleDigitInt(file, value.hour);
+  file.writeByteSync(colon);
+  _writeDoubleDigitInt(file, value.minute);
+  file.writeByteSync(colon);
+  _writeDoubleDigitInt(file, value.second);
+  file.writeByteSync(comma);
+  _writeDoubleDigitInt(file, value.millisecond);
+}
+
+void export(RandomAccessFile file, Iterable<Subtitle> subs) {
+  const newline = 0x0a; // \n
+  for (final (i, v) in subs.indexed) {
+    if (i != 0) {
+      file.writeByteSync(newline);
+    }
+
+    file.writeStringSync(i.toString());
+    file.writeByteSync(newline);
+
+    _writeTime(file, v.start.format());
+    file.writeStringSync(" --> ");
+    _writeTime(file, v.end.format());
+    file.writeByteSync(newline);
+
+    file.writeStringSync(v.text);
+    file.writeByteSync(newline);
+  }
+}

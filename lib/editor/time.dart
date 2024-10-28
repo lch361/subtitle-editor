16417316b75bec3b2f74c0252fc1c117f7d95144
lch361 <author@lch361.net>
2024-10-28 03:13:53 +0300

    Добавил миллисекундам формат - разбиение на час, минуту, секунду и миллисекунду.

--- lib/editor/time.dart
+++ lib/editor/time.dart
@@ -1,22 +1,37 @@
+class MillisFormatted {
+  final int hour, minute, second, millisecond;
+
+  MillisFormatted(Millis millis)
+      : hour = millis.hours,
+        minute = millis.minutes % 60,
+        second = millis.seconds % 60,
+        millisecond = millis.ticks % 1000;
+}
+
 /// Целое, всегда положительное число, выражающее количество миллисекунд.
 class Millis implements Comparable<Millis> {
-  final int _ticks;
-  int get ticks => _ticks;
+  final int ticks;
+
+  int get seconds => ticks ~/ 1000;
+  int get minutes => seconds ~/ 60;
+  int get hours => minutes ~/ 60;
 
   /// # Инварианты
   /// ```dart
   /// assert(this.ticks >= 0);
   /// ```
-  Millis(this._ticks) {
+  Millis(this.ticks) {
     assert(ticks >= 0);
   }
 
-  Millis.clamp(int value) : _ticks = value < 0 ? 0 : value;
+  Millis.clamp(int value) : ticks = value < 0 ? 0 : value;
 
   @override
   int compareTo(Millis other) {
     return ticks.compareTo(other.ticks);
   }
+
+  MillisFormatted format() => MillisFormatted(this);
 
   bool operator <(Millis other) => ticks < other.ticks;
   bool operator >(Millis other) => ticks > other.ticks;

6a183962b59e10095ed9c3b2d4084b30f4145429
NeonNik2245 <kromout2245@icloud.com>
2024-11-18 01:43:58 +0300

    Добавление видеоплеера
    Добавлен видеоплеер, кнопки под него (весьма немного), разделение экрана под размер окна

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+import 'package:media_kit/media_kit.dart';                      // Provides [Player], [Media], [Playlist] etc.
+import 'package:media_kit_video/media_kit_video.dart';          // Provides [VideoController] & [Video] etc.
+import 'package:file_picker/file_picker.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Обязательная инициализации пакета media kit
+  MediaKit.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -57,6 +66,56 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  /////////////////////////////////////////////
+  // Создаём плеер и управление плейером
+  // Create a [Player] to control playback.
+  late Player player = Player();
+  // Create a [VideoController] to handle video output from [Player].
+  late VideoController controller = VideoController(player);
+
+  // Выбранный файл-видео
+  late File? _file_video;
+  late File? _file_sub;
+
+  void getFileVideo() async {
+   FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: FileType.video,
+    //allowedExtensions: ['mp4', 'mov', 'avi', 'wmv'],
+   );
+ 
+   if (result != null) {
+    String _videofilePath = result.files.single.path!;
+    print(_videofilePath);
+    player.open(Media(_videofilePath));
+     setState(() {});
+   } else {
+     // User canceled the picker
+     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+       content: Text('Файл не выбран'),
+     ));
+   }
+ }
+
+ void getFileSubtitle() async {
+   FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: FileType.custom,
+    allowedExtensions: ['txt', 'srt'],
+   );
+ 
+   if (result != null) {
+    _file_sub = File(result.files.single.path!);
+    setState(() {});
+   } else {
+     // User canceled the picker
+     // You can show snackbar or fluttertoast
+     // here like this to show warning to user
+     // ignore: use_build_context_synchronously
+     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+       content: Text('Please select file'),
+     ));
+   }
+  }
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -66,6 +125,28 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  ///////////////////////////////////////////////////
+  // Функция для вставки видео в плейер
+  @override
+  void initState() {
+    super.initState();
+    // Play a [Media] or [Playlist].
+    player.open(Media('https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4'));
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  void _tellTime() {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+       content: Text("Video is on ${player.state.position}"),
+     ));
+    //print(player.state.position);
   }
 
   @override
@@ -84,43 +165,94 @@ class _MyHomePageState extends State<MyHomePage> {
       body: 
       Row( //Тело, разделённое по колонкам
       children: [
-        SizedBox(
-          width: 240,
-          child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            SizedBox(
-              width: 120,
-              height: 120,
-              child: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
+        
+        Column(children: [
+          
+          SizedBox( // Коробка под видео
+            width: MediaQuery.sizeOf(context).width * 0.7,
+            //width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width * 0.7 * 9.0 / 16.0,
+            // Use [Video] widget to display video output.
+            child: Video(controller: controller),
+          ),
+
+          Row( // Для кнопок нужно разделить пространство по столбикам
+            children: [
+
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: FloatingActionButton(
+                  onPressed: _tellTime,
+                  tooltip: 'Tells the time',
+                  child: const Icon(Icons.access_time_outlined),
+                ),
               ),
-            ),
-          ],
-        ),
-        ),
+
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: FloatingActionButton(
+                  onPressed: getFileVideo,
+                  tooltip: 'Choose video file',
+                  child: const Icon(Icons.video_call_rounded),
+                ),
+              ),
+
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: FloatingActionButton(
+                  onPressed: getFileSubtitle,
+                  tooltip: 'Choose video file',
+                  child: const Icon(Icons.text_snippet_rounded),
+                ),
+              ),
+
+            ],
+            
+          ),
+
+        ],),
+
+
+        //   child: Column(
+        //   // Column is also a layout widget. It takes a list of children and
+        //   // arranges them vertically. By default, it sizes itself to fit its
+        //   // children horizontally, and tries to be as tall as its parent.
+        //   //
+        //   // Column has various properties to control how it sizes itself and
+        //   // how it positions its children. Here we use mainAxisAlignment to
+        //   // center the children vertically; the main axis here is the vertical
+        //   // axis because Columns are vertical (the cross axis would be
+        //   // horizontal).
+        //   //
+        //   // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        //   // action in the IDE, or press "p" in the console), to see the
+        //   // wireframe for each widget.
+        //   //mainAxisAlignment: MainAxisAlignment.start,
+        //   children: <Widget>[
+        //     SizedBox(
+        //       width: MediaQuery.of(context).size.width,
+        //       height: MediaQuery.of(context).size.width * 9.0 / 16.0,
+        //       // Use [Video] widget to display video output.
+        //       child: Video(controller: controller),
+        //     ),
+        //     // Text(
+        //     //   '$_counter',
+        //     //   style: Theme.of(context).textTheme.headlineMedium,
+        //     // ),
+        //     // SizedBox(
+        //     //   width: 120,
+        //     //   height: 120,
+        //     //   child: FloatingActionButton(
+        //     //     onPressed: _incrementCounter,
+        //     //     tooltip: 'Increment',
+        //     //     child: const Icon(Icons.add),
+        //     //   ),
+        //     // ),
+        //   ],
+        // ),
         Container(width: 5, color: Colors.black),
         Expanded(
           child: ListView.builder( //Субтитры
